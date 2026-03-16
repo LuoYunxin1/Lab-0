76d4fb259ec85385ffd9cf6d573d86a1de2fe0e1
Luoyunxin <202583890018@nuist.edu.cn>
2026-03-16 21:50:11 +0800

    add 1.5 and 6.3

---
+++ addnumbers1.py
@@ -0,0 +1,10 @@
+#add1.5 and 6.3
+#Luoyunxin
+#3/16
+num1 = 1.5 
+num2 = 6.3 
+# Add two numbers 
+sum = num1 + num2 
+# Display the sum 
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum)) 
+
