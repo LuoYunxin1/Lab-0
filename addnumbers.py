34e22a61bd8e00d99a42732580ab9e1bbeed6402
Luoyunxin <202583890018@nuist.edu.cn>
2026-03-16 21:41:08 +0800

    add two number

---
+++ addnumbers.py
@@ -0,0 +1,7 @@
+#add two numbers
+#Luoyunxin
+
+num1 = float(input("please enter the first number："))
+num2 = float(input("please enter the second number："))
+sum_result = num1 + num2
+print ("the sum : " , sum_result)
